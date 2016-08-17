62d1902286939349c72d7032515031ba8d3ac686
Michael Gentili <gentili@mcpnet.ca>
2016-08-17 12:30:11 -0400

    Working loop

--- dslcheck.py
+++ dslcheck.py
@@ -28,6 +28,13 @@ addrregex = r"addr:(\d+\.\d+\.\d+\.\d+)"
 
 client = BridgeClient()
 
+leds = [0,0,0,0,0,0,0]
+
+# 0 = OFF
+# 1 = ON
+# 2 = HALFDUTY
+# 3 = FLASH
+
 #print "Sending text to mailbox"
 #client.mailbox("Hi there")
 
@@ -45,40 +52,79 @@ def try_command(step, errname, commandline):
         print errname
         raise DSLCheckException(step)
 
+def led_set(step,value):
+    leds[step] = value
+    led_tx()
+
+def led_clear_set(step,value):
+    for i in range(step,len(leds)):
+        leds[i] = 0
+    leds[step] = value
+    led_tx()
+
+def led_tx():
+    msg = '[';
+    for i in leds:
+        msg += str(i)
+    msg += ']'
+    print msg
+
+
 while True:
     try:
         curstep = 0
         print "Check if wifi is running"
+        led_set(curstep,2)
         retval = try_command(curstep,"ERR: wifi interface missing", ["ifconfig", wifidevice])
+        time.sleep(1)
+        led_set(curstep,1)
         curstep += 1
 
         print "Check if ip address is assigned"
+        led_set(curstep,2)
         match = re.search(addrregex,retval)
         if not match:
             print "ERR: no ip address assigned to wifi"
             raise DSLCheckException(curstep)
-        localip = match.group(1)
+        time.sleep(1)
+        led_set(curstep,1)
         curstep += 1
 
         print "Check if we can ping the access point"
+        led_set(curstep,2)
         retval = try_command(curstep,"ERR: can't ping access point", ["ping","-q","-c","1","-W","4",apaddr])
+        time.sleep(1)
+        led_set(curstep,1)
         curstep += 1
 
         print "Check if we can ping the border gateway internal address"
+        led_set(curstep,2)
         retval = try_command(curstep,"ERR: can't ping border gateway private address", ["ping","-q","-c","1","-W","4",gatewayprivateaddr])
+        time.sleep(1)
+        led_set(curstep,1)
         curstep += 1
 
         print "Check if we can ping the border gateway public address"
+        led_set(curstep,2)
         retval = try_command(curstep,"ERR: can't ping border gateway public address", ["ping","-q","-c","1","-W","4",gatewaypublicaddr])
+        time.sleep(1)
+        led_set(curstep,1)
         curstep += 1
 
         print "Check if we can ping the ptp peer"
+        led_set(curstep,2)
         retval = try_command(curstep,"ERR: can't ping border gateway peer address", ["ping","-q","-c","1","-W","4",gatewaypeeraddr])
+        time.sleep(1)
+        led_set(curstep,1)
         curstep += 1
 
         print "Check if we can ping remote address"
+        led_set(curstep,2)
         retval = try_command(curstep,"ERR: can't ping remote address", ["ping","-q","-c","1","-W","4",remoteaddr])
+        time.sleep(1)
+        led_set(curstep,1)
     except DSLCheckException as e:
         print "Failed check {}".format(e.getstep())
+        led_clear_set(e.getstep(),3)
 
-    time.sleep(1)
+    time.sleep(5)
